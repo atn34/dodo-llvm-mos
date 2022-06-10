eb62f273d76ef0d42f6d3a455aa9fc15995f3d91
Andrew Noyes <anoyes34@gmail.com>
2022-06-09 20:07:46 -0700

    Remove .ident since this has been edited

--- start.s
+++ start.s
@@ -16,6 +16,5 @@ _start:                                 ; @_start
 .Lfunc_end0:
 	.size	_start, .Lfunc_end0-_start
                                         ; -- End function
-	.ident	"clang version 15.0.0 (https://www.github.com/llvm-mos/llvm-mos.git e08278f771d06cdd667e58ebf5b18f4c5066a250)"
 	.section	".note.GNU-stack","",@progbits
 	.addrsig
