a5b8b4040ac539d953124f4397a219cae1bb3312
Andrew Noyes <anoyes34@gmail.com>
2022-07-02 15:14:05 -0700

    Global state

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include "api.h"
 
+#include <string.h>
+
 #define SCREEN_WIDTH_BEGIN 0
 #define SCREEN_HEIGHT_BEGIN 0
 #define SCREEN_WIDTH_END 128
@@ -26,7 +28,7 @@ struct GameState {
   int lastblock_x;
   int last_block_x;
   int last_block_y;
-};
+} state;
 
 static char *const tile_sprites[] = {
     "\x00\x00\x00\x00\x00\x00\x00\x00",
@@ -42,67 +44,67 @@ static char *const block_sprites[] = {
     "\xff\xff\xff\xff\xff\xff\xff\xff",
 };
 
-static void processInput(struct GameState *current, byte buttons) {
-  if ((buttons & 16) == 0 && (current->last_buttons & 16) != 0) {
-    current->block_want_jump = 1;
+static void processInput(byte buttons) {
+  if ((buttons & 16) == 0 && (state.last_buttons & 16) != 0) {
+    state.block_want_jump = 1;
   }
-  if (current->block_want_jump && current->block_moves_available > 0) {
+  if (state.block_want_jump && state.block_moves_available > 0) {
     // Begin jump
-    current->block_want_jump = 0;
-    current->block_velocity_y = -6;
-    current->block_jump_frames_left = 8;
-    current->block_moves_available -= 1;
+    state.block_want_jump = 0;
+    state.block_velocity_y = -6;
+    state.block_jump_frames_left = 8;
+    state.block_moves_available -= 1;
   }
-  if ((buttons & 16) == 0 && (current->last_buttons & 16) == 0 &&
-      current->block_jump_frames_left > 0) {
+  if ((buttons & 16) == 0 && (state.last_buttons & 16) == 0 &&
+      state.block_jump_frames_left > 0) {
     // Continue jump
-    current->block_velocity_y = -6;
-    current->block_jump_frames_left--;
+    state.block_velocity_y = -6;
+    state.block_jump_frames_left--;
   }
-  if ((buttons & 16) != 0 && (current->last_buttons & 16) == 0) {
+  if ((buttons & 16) != 0 && (state.last_buttons & 16) == 0) {
     // End jump
-    current->block_jump_frames_left = 0;
-    current->block_want_jump = 0;
+    state.block_jump_frames_left = 0;
+    state.block_want_jump = 0;
   }
   if ((buttons & 8) == 0) {
     // Move right
-    if (current->block_velocity_x < 2) {
-      current->block_velocity_x = 2;
+    if (state.block_velocity_x < 2) {
+      state.block_velocity_x = 2;
     } else {
-      current->block_velocity_x = 4;
+      state.block_velocity_x = 4;
     }
   } else if ((buttons & 4) == 0) {
     // Move left
-    if (current->block_velocity_x > -2) {
-      current->block_velocity_x = -2;
+    if (state.block_velocity_x > -2) {
+      state.block_velocity_x = -2;
     } else {
-      current->block_velocity_x = -4;
+      state.block_velocity_x = -4;
     }
   } else {
-    current->block_velocity_x = 0;
+    state.block_velocity_x = 0;
   }
-  if ((buttons & 32) == 0 && (current->last_buttons & 32) != 0 &&
-      current->block_moves_available > 0) {
+  if ((buttons & 32) == 0 && (state.last_buttons & 32) != 0 &&
+      state.block_moves_available > 0) {
     if ((buttons & 8) == 0) {
       // Dash right
-      current->block_velocity_y = 0;
-      current->block_moves_available -= 1;
-      current->block_x += 24;
+      state.block_velocity_y = 0;
+      state.block_moves_available -= 1;
+      state.block_x += 24;
     }
     if ((buttons & 4) == 0) {
       // Dash left
-      current->block_velocity_y = 0;
-      current->block_moves_available -= 1;
-      current->block_x -= 24;
+      state.block_velocity_y = 0;
+      state.block_moves_available -= 1;
+      state.block_x -= 24;
     }
   }
-  if (current->block_moves_available < 0) {
-    current->block_moves_available = 0;
+  if (state.block_moves_available < 0) {
+    state.block_moves_available = 0;
   }
-  current->last_buttons = buttons;
+  state.last_buttons = buttons;
 }
 
-static int handlePhysics(struct GameState *current) {
+static int handlePhysics() {
   int i;
   int j;
   int i_end;
@@ -110,236 +112,230 @@ static int handlePhysics(struct GameState *current) {
   int last_x;
   int last_y;
   int intersects_blocks = 0;
-  current->block_velocity_y += 1;
-  current->block_y += current->block_velocity_y >> 2;
-  current->block_x += current->block_velocity_x;
+  state.block_velocity_y += 1;
+  state.block_y += state.block_velocity_y >> 2;
+  state.block_x += state.block_velocity_x;
 
-  if (current->block_x < SCREEN_WIDTH_BEGIN) {
-    current->block_x = SCREEN_WIDTH_BEGIN;
+  if (state.block_x < SCREEN_WIDTH_BEGIN) {
+    state.block_x = SCREEN_WIDTH_BEGIN;
   }
-  if (current->block_y < SCREEN_HEIGHT_BEGIN) {
-    current->block_y = SCREEN_HEIGHT_BEGIN;
-    current->block_velocity_y = 0;
+  if (state.block_y < SCREEN_HEIGHT_BEGIN) {
+    state.block_y = SCREEN_HEIGHT_BEGIN;
+    state.block_velocity_y = 0;
   }
-  if (current->block_x + BLOCK_WIDTH > SCREEN_WIDTH_END) {
-    current->block_x = SCREEN_WIDTH_END - BLOCK_WIDTH;
+  if (state.block_x + BLOCK_WIDTH > SCREEN_WIDTH_END) {
+    state.block_x = SCREEN_WIDTH_END - BLOCK_WIDTH;
   }
-  if (current->block_y + BLOCK_HEIGHT > SCREEN_HEIGHT_END) {
-    current->block_y = SCREEN_HEIGHT_END - BLOCK_HEIGHT;
-    current->block_velocity_y = 0;
+  if (state.block_y + BLOCK_HEIGHT > SCREEN_HEIGHT_END) {
+    state.block_y = SCREEN_HEIGHT_END - BLOCK_HEIGHT;
+    state.block_velocity_y = 0;
   }
-  last_x = current->last_block_x;
-  last_y = current->last_block_y;
-  i_end = current->block_y / BLOCK_HEIGHT + 1 + !!(current->block_y & 7);
-  j_end = current->block_x / BLOCK_WIDTH + 1 + !!(current->block_x & 7);
-  for (i = current->block_y / BLOCK_HEIGHT; i < i_end; ++i) {
-    for (j = current->block_x / BLOCK_WIDTH; j < j_end; ++j) {
+  last_x = state.last_block_x;
+  last_y = state.last_block_y;
+  i_end = state.block_y / BLOCK_HEIGHT + 1 + !!(state.block_y & 7);
+  j_end = state.block_x / BLOCK_WIDTH + 1 + !!(state.block_x & 7);
+  for (i = state.block_y / BLOCK_HEIGHT; i < i_end; ++i) {
+    for (j = state.block_x / BLOCK_WIDTH; j < j_end; ++j) {
       // Indices of all tiles intersecting block
-      if (current->tiles[i][j] == 1) {
+      if (state.tiles[i][j] == 1) {
         intersects_blocks = 1;
-        current->block_moves_available = 2;
+        state.block_moves_available = 2;
         if (last_x / 8 + !!(last_x & 7) < j &&
-            current->block_x / 8 + !!(current->block_x & 7) == j) {
-          current->block_x = j * 8 - 8;
-        } else if (last_x / 8 > j && current->block_x / 8 == j) {
-          current->block_x = current->block_x / 8 * 8 + 8;
+            state.block_x / 8 + !!(state.block_x & 7) == j) {
+          state.block_x = j * 8 - 8;
+        } else if (last_x / 8 > j && state.block_x / 8 == j) {
+          state.block_x = state.block_x / 8 * 8 + 8;
         }
       }
     }
   }
   if (intersects_blocks) {
-    i_end = current->block_y / BLOCK_HEIGHT + 1 + !!(current->block_y & 7);
-    j_end = current->block_x / BLOCK_WIDTH + 1 + !!(current->block_x & 7);
-    for (i = current->block_y / BLOCK_HEIGHT; i < i_end; ++i) {
-      for (j = current->block_x / BLOCK_WIDTH; j < j_end; ++j) {
+    i_end = state.block_y / BLOCK_HEIGHT + 1 + !!(state.block_y & 7);
+    j_end = state.block_x / BLOCK_WIDTH + 1 + !!(state.block_x & 7);
+    for (i = state.block_y / BLOCK_HEIGHT; i < i_end; ++i) {
+      for (j = state.block_x / BLOCK_WIDTH; j < j_end; ++j) {
         // Indices of all tiles intersecting block
-        if (current->tiles[i][j] == 1) {
-          current->block_moves_available = 2;
+        if (state.tiles[i][j] == 1) {
+          state.block_moves_available = 2;
           if (last_y / 8 + !!(last_y & 7) < i &&
-              current->block_y / 8 + !!(current->block_y & 7) == i) {
-            current->block_y = i * 8 - 8;
-            current->block_velocity_y = 0;
-          } else if (last_y / 8 > i && current->block_y / 8 == i) {
-            current->block_y = current->block_y / 8 * 8 + 8;
-            current->block_velocity_y = 0;
+              state.block_y / 8 + !!(state.block_y & 7) == i) {
+            state.block_y = i * 8 - 8;
+            state.block_velocity_y = 0;
+          } else if (last_y / 8 > i && state.block_y / 8 == i) {
+            state.block_y = state.block_y / 8 * 8 + 8;
+            state.block_velocity_y = 0;
           }
         }
       }
     }
   }
-  i_end = current->block_y / BLOCK_HEIGHT + 1 + !!(current->block_y & 7);
-  j_end = current->block_x / BLOCK_WIDTH + 1 + !!(current->block_x & 7);
-  for (i = current->block_y / BLOCK_HEIGHT; i < i_end; ++i) {
-    for (j = current->block_x / BLOCK_WIDTH; j < j_end; ++j) {
+  i_end = state.block_y / BLOCK_HEIGHT + 1 + !!(state.block_y & 7);
+  j_end = state.block_x / BLOCK_WIDTH + 1 + !!(state.block_x & 7);
+  for (i = state.block_y / BLOCK_HEIGHT; i < i_end; ++i) {
+    for (j = state.block_x / BLOCK_WIDTH; j < j_end; ++j) {
       // Indices of all tiles intersecting block
-      if (current->tiles[i][j] == 2) {
+      if (state.tiles[i][j] == 2) {
         return 1;
-      } else if (current->tiles[i][j] == 3) {
-        DRAW_SPRITE((byte *)tile_sprites[current->tiles[i][j]], j * 8, i * 8, 8,
-                    8,
+      } else if (state.tiles[i][j] == 3) {
+        DRAW_SPRITE((byte *)tile_sprites[state.tiles[i][j]], j * 8, i * 8, 8, 8,
                     /*flip*/ 0, DRAW_XOR);
-        if (--current->coins_remaining == 0) {
+        if (--state.coins_remaining == 0) {
           return 2;
         }
-        current->tiles[i][j] = 0;
+        state.tiles[i][j] = 0;
       }
     }
   }
-  current->last_block_x = current->block_x;
-  current->last_block_y = current->block_y;
+  state.last_block_x = state.block_x;
+  state.last_block_y = state.block_y;
   return 0;
 }
 
-static void drawBlock(struct GameState *current) {
-  DRAW_SPRITE((byte *)block_sprites[current->block_moves_available],
-              current->block_x, current->block_y, BLOCK_WIDTH, BLOCK_HEIGHT,
+static void drawBlock() {
+  DRAW_SPRITE((byte *)block_sprites[state.block_moves_available], state.block_x,
+              state.block_y, BLOCK_WIDTH, BLOCK_HEIGHT,
               /*flip*/ 0, DRAW_OR);
 }
 
-static void eraseBlock(struct GameState *current) {
-  DRAW_SPRITE((byte *)block_sprites[current->block_moves_available],
-              current->block_x, current->block_y, BLOCK_WIDTH, BLOCK_HEIGHT,
+static void eraseBlock() {
+  DRAW_SPRITE((byte *)block_sprites[state.block_moves_available], state.block_x,
+              state.block_y, BLOCK_WIDTH, BLOCK_HEIGHT,
               /*flip*/ 0, DRAW_XOR);
 }
 
-static void drawTiles(struct GameState *current) {
+static void drawTiles() {
   int i;
   int j;
   for (i = 0; i < TILE_ROWS; ++i) {
     for (j = 0; j < TILE_COLUMNS; ++j) {
-      if (current->tiles[i][j] == 3) {
-        ++current->coins_remaining;
+      if (state.tiles[i][j] == 3) {
+        ++state.coins_remaining;
       }
-      DRAW_SPRITE((byte *)tile_sprites[current->tiles[i][j]], j * 8, i * 8, 8,
-                  8,
+      DRAW_SPRITE((byte *)tile_sprites[state.tiles[i][j]], j * 8, i * 8, 8, 8,
                   /*flip*/ 0, DRAW_OR);
     }
   }
 }
 
-static void initGameState(struct GameState *current) {
-  int i;
-  // Memset 0
-  for (i = 0; i < (int)sizeof(struct GameState); ++i) {
-    ((char *)current)[i] = 0;
-  }
-  current->block_moves_available = 2;
+static void initGameState() {
+  memset(&state, 0, sizeof(state));
+  state.block_moves_available = 2;
 }
 
-static void initLevel1(struct GameState *current) {
+static void initLevel1() {
   int i;
   int j;
-  initGameState(current);
+  initGameState();
 
   for (i = 0; i < TILE_ROWS; ++i) {
     for (j = 0; j < TILE_COLUMNS; ++j) {
-      current->tiles[i][j] = 3;
+      state.tiles[i][j] = 3;
     }
   }
 
   for (i = 0; i < TILE_ROWS; ++i) {
-    current->tiles[i][0] = 1;
-    current->tiles[i][15] = 1;
+    state.tiles[i][0] = 1;
+    state.tiles[i][15] = 1;
   }
 
   for (j = 0; j < TILE_COLUMNS; ++j) {
-    current->tiles[0][j] = 1;
-    current->tiles[7][j] = 1;
+    state.tiles[0][j] = 1;
+    state.tiles[7][j] = 1;
   }
 
-  current->block_x = 12;
-  current->block_y = 8;
+  state.block_x = 12;
+  state.block_y = 8;
 }
 
-static void initLevel2(struct GameState *current) {
+static void initLevel2() {
   int j;
-  initLevel1(current);
+  initLevel1();
 
   for (j = 0; j < 4; ++j) {
-    current->tiles[j][7] = 2;
-    current->tiles[j + 4][8] = 2;
+    state.tiles[j][7] = 2;
+    state.tiles[j + 4][8] = 2;
   }
 }
 
-static void initLevel3(struct GameState *current) {
+static void initLevel3() {
   int i;
-  initLevel1(current);
+  initLevel1();
 
   for (i = 0; i < TILE_COLUMNS; ++i) {
-    current->tiles[7][i] = 2;
+    state.tiles[7][i] = 2;
   }
 }
 
-static void initLevel4(struct GameState *current) {
+static void initLevel4() {
   int i;
-  initLevel3(current);
+  initLevel3();
 
   for (i = 0; i < TILE_COLUMNS; ++i) {
-    current->tiles[0][i] = 2;
+    state.tiles[0][i] = 2;
   }
 }
 
-static void initLevel5(struct GameState *current) {
+static void initLevel5() {
   int i;
   int j;
-  initLevel4(current);
+  initLevel4();
 
   for (i = 0; i < TILE_COLUMNS; ++i) {
-    current->tiles[0][i] = 2;
+    state.tiles[0][i] = 2;
   }
   for (j = 0; j < TILE_ROWS; ++j) {
-    current->tiles[j][0] = 2;
-    current->tiles[j][15] = 2;
+    state.tiles[j][0] = 2;
+    state.tiles[j][15] = 2;
   }
   for (j = 4; j < 12; ++j) {
-    current->tiles[3][j] = 1;
-    current->tiles[4][j] = 1;
+    state.tiles[3][j] = 1;
+    state.tiles[4][j] = 1;
   }
 }
 
-static void initLevel6(struct GameState *current) {
+static void initLevel6() {
   int j;
-  initLevel5(current);
+  initLevel5();
 
   for (j = 0; j < TILE_ROWS; ++j) {
-    current->tiles[j][7] = 2;
-    current->tiles[j][8] = 2;
+    state.tiles[j][7] = 2;
+    state.tiles[j][8] = 2;
   }
 }
 
-static void initLevel7(struct GameState *current) {
-  initLevel6(current);
+static void initLevel7() {
+  initLevel6();
 
-  current->tiles[1][6] = 2;
-  current->tiles[2][6] = 2;
-  current->tiles[1][9] = 2;
-  current->tiles[2][9] = 2;
+  state.tiles[1][6] = 2;
+  state.tiles[2][6] = 2;
+  state.tiles[1][9] = 2;
+  state.tiles[2][9] = 2;
 }
 
-static void initLevel8(struct GameState *current) {
+static void initLevel8() {
   int i;
   int j;
-  initLevel1(current);
+  initLevel1();
   for (i = 0; i < TILE_ROWS; ++i) {
-    current->tiles[i][0] = 2;
-    current->tiles[i][15] = 2;
+    state.tiles[i][0] = 2;
+    state.tiles[i][15] = 2;
   }
   for (j = 3; j < TILE_COLUMNS - 3; ++j) {
-    current->tiles[3][j] = 2;
-    current->tiles[4][j] = 2;
+    state.tiles[3][j] = 2;
+    state.tiles[4][j] = 2;
   }
   for (j = 0; j < TILE_COLUMNS; ++j) {
-    current->tiles[0][j] = 2;
-    current->tiles[7][j] = 2;
+    state.tiles[0][j] = 2;
+    state.tiles[7][j] = 2;
   }
-  current->tiles[3][0] = 1;
-  current->tiles[4][0] = 1;
-  current->tiles[3][15] = 1;
-  current->tiles[4][15] = 1;
-  current->tiles[0][7] = 1;
-  current->tiles[0][8] = 1;
-  current->tiles[7][7] = 1;
-  current->tiles[7][8] = 1;
+  state.tiles[3][0] = 1;
+  state.tiles[4][0] = 1;
+  state.tiles[3][15] = 1;
+  state.tiles[4][15] = 1;
+  state.tiles[0][7] = 1;
+  state.tiles[0][8] = 1;
+  state.tiles[7][7] = 1;
+  state.tiles[7][8] = 1;
 }
 
 static void (*levels[])(struct GameState *) = {
@@ -353,7 +349,6 @@ int main() {
   int i;
   int status;
   int level = 0;
-  struct GameState state;
 
   // Initialize the API
   api_init();
@@ -379,14 +374,14 @@ BEGIN_GAME:
   // Clear the graphics in video memory
   CLEAR();
 
-  drawTiles(&state);
+  drawTiles();
 
   for (;;) {
-    processInput(&state, READ_BUTTONS());
+    processInput(READ_BUTTONS());
 
-    status = handlePhysics(&state);
+    status = handlePhysics();
 
-    drawBlock(&state);
+    drawBlock();
 
     // Push video memory to the OLED
     DISPLAY();
@@ -394,7 +389,7 @@ BEGIN_GAME:
     // Wait for next interrupt
     WAIT();
 
-    eraseBlock(&state);
+    eraseBlock();
 
     if (status == 1) {
       goto BEGIN_GAME;
@@ -406,4 +401,3 @@ BEGIN_GAME:
   }
   return 0;
 }
-
